e8a20d3822b274b5e2bdb84145ecbd36b00a6124
Vic-Nas <nasci.victorio@gmail.com>
2026-01-26 15:58:39 -0500

    Fixing urls

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.urls import re_path
 from views import proxy_view, home
 
+def not_found(request):
+    return HttpResponseNotFound("Not found")
+
 urlpatterns = [
+    re_path(r'^(favicon\.ico|robots\.txt)$', not_found),  # Block common bot files
     re_path(r'^(?P<service>[^/]+)/(?P<path>.*)$', proxy_view),
     re_path(r'^$', home),
 ]
